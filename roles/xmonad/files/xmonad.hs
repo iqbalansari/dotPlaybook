043d4745d77aa16b4e42b9b993c5836d0eb133b1
Iqbal Ansari <iqbal.ansari@ignitesol.com>
2015-06-26 16:42:21 +0530

    Revert "[xmonad] s/firefox/conkeror"
    This reverts commit b8d5b780ab6ba17f6c7a6a7a7c1aaa1dc8f230ae.

--- roles/xmonad/files/xmonad.hs
+++ roles/xmonad/files/xmonad.hs
@@ -124,7 +124,7 @@ myManagementHooks = composeOne [
   className                =? "Display"                                           -?> doCenterFloat,
   className                =? "Artha"                                             -?> doCenterFloat,
   className                =? "Emacs"                                             -?> doShift "emacs",
-  className                =? "Conkeror"                                          -?> doShift "web",
+  className                =? "Firefox"                                           -?> doShift "web",
   className                =? "Eog"                                               -?> doCenterFloat
   ] <+> composeAll [
   className =? "qemu-system-x86_64" --> doShift "vm",
