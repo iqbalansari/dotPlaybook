d6f0ab88579268a34959ab83604f76136e44eea9
Iqbal Ansari <iqbalansari02@yahoo.com>
2019-06-20 22:34:30 +0530

    [run.sh] Install psutil along with ansible, dependency for dconf module

--- run.sh
+++ run.sh
@@ -283,6 +283,12 @@ install_ansible () {
         .venv/bin/pip install --ignore-installed ansible=="$ANSIBLE_VERSION"
         log "ansible installed inside virtualenv" change high
     fi
+
+    log "Installing psutil inside virtualenv ... " info high
+    if ! (.venv/bin/pip freeze | grep -q psutil) ; then
+        .venv/bin/pip install psutil
+        log "psutil installed inside virtualenv" change high
+    fi
 }
 
 run_ansible () {
